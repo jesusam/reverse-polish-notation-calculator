424864c16411657bfd16605d59eb46095f318ff0
Andrew Copp <andrew.w.copp@gmail.com>
2013-05-21 11:44:53 -0400

    (20/20) Added CalculatorViewController and the matching test file. Notice the include objc/runtime.h library. This will help test view controllers.

---
+++ ReversePolishNotationCalculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  ReversePolishNotationCalculator
+//
+//  Created by Andrew Copp on 5/21/13.
+//  Copyright (c) 2013 Jesus Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
---
+++ ReversePolishNotationCalculator/CalculatorViewController.m
@@ -0,0 +1,38 @@
+//
+//  CalculatorViewController.m
+//  ReversePolishNotationCalculator
+//
+//  Created by Andrew Copp on 5/21/13.
+//  Copyright (c) 2013 Jesus Morales. All rights reserved.
+//
+
+#import "CalculatorViewController.h"
+
+@interface CalculatorViewController ()
+
+@end
+
+@implementation CalculatorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
